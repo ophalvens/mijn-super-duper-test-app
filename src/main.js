5b162ea83c224ff07e6e63c526a5c65d3c52a032
Steven Ophalvens <ophalvens@users.noreply.github.com>
2022-10-11 19:23:17 +0200

    import en use VueAxios en axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router';
 
 import { IonicVue } from '@ionic/vue';
 
+// Axios toevoegen
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
@@ -25,7 +29,8 @@ import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(VueAxios, axios);
   
 router.isReady().then(() => {
   app.mount('#app');
