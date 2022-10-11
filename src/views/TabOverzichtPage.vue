0e65148b8c25c424136a5b22ff70276d6ebcf706
Steven Ophalvens <ophalvens@users.noreply.github.com>
2022-10-11 20:54:03 +0200

    PRODUCTSget.php met axios
    * oude producten lijst verwijderd
    * naam properties van producten aangepast naar de properties in de response
    * opvragen van de API zelf
    * weergeven van de producten in lijstvorm

--- src/views/TabOverzichtPage.vue
+++ src/views/TabOverzichtPage.vue
@@ -13,10 +13,10 @@
       </ion-header>
 
       <ion-list>
-        <ion-item v-for="{id, pr_prijs, pr_naam, ct_naam} in producten" :key="id">
-          <ion-item slot="start">€{{pr_prijs}}</ion-item>
-          <ion-label>{{pr_naam}}</ion-label>
-          <ion-item slot="end">{{ct_naam}}</ion-item>
+        <ion-item v-for="{PR_ID, PR_prijs, PR_naam, CT_OM} in producten" :key="PR_ID">
+          <ion-item slot="start">€{{PR_prijs}}</ion-item>
+          <ion-label>{{PR_naam}}</ion-label>
+          <ion-item slot="end">{{CT_OM}}</ion-item>
         </ion-item>
 
       </ion-list>
@@ -26,13 +26,45 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel, IonItem } from '@ionic/vue';
+import { ref, inject } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonLabel, IonItem, onIonViewWillEnter } from '@ionic/vue';
 
-const producten = ref([
-  {pr_id:1, pr_naam: "Banaan", pr_prijs: 1.50, ct_naam: "fruit", pr_ct_id: 1},
-  {pr_id:2, pr_naam: "Spruitjes", pr_prijs: 3.50, ct_naam: "groente", pr_ct_id: 2},
-  {pr_id:3, pr_naam: "Sinaasappel", pr_prijs: 1.00, ct_naam: "fruit", pr_ct_id: 1}
-]);
+const producten = ref([]);
 
+
+const axios = inject('axios') // inject axios
+
+const getProducten = () => {
+  axios
+    .post('https://stevenop.be/wm/api/PRODUCTSget.php')
+    .then(response => {
+      // controleer de response
+      if(response.status !== 200) {
+        // er is iets fout gegaan, doe iets met deze info
+        console.log(response.status);
+      }
+      if(!response.data.data){
+        // De data die we verwachten zit niet in response.data :
+        // de aangesproken API stopt zijn data in een data object (eigen code).
+        // Deze data zit echter ook in het data object in response.
+        // Daarom dus response.data.data
+        console.log('response.data.data is not ok');
+        return;
+      }
+      console.log(response.date);
+
+      // We gaan eerst de lijst leeg maken, een beetje ruw, maar werkt op dit moment
+      producten.value = [];
+      for(let i = 0, end = response.data.data.length; i < end; i++){
+        // response.data.data is een array, we lopen er door en stoppen de gegevens van iedere record in producten.value (ref!)
+        producten.value.push(response.data.data[i]);
+      }
+    }) ;
+}
+
+onIonViewWillEnter(() => {
+  // we halen de producten op bij het laden van dit scherm
+  // er zijn strategieën mogelijk om dit meer performant te cachen
+  getProducten();
+});
 </script>
